c21719b7943308eda33fc743b64febba623ee79a
Tabizzz <tabatha.reds@gmail.com>
2022-09-03 01:20:25 -0500

    Create PointEmitter.cs

---
+++ Emitters/PointEmitter.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Microsoft.Xna.Framework;
+namespace KokoLib.Emitters;
+
+class PointEmitter : ModHandlerEmitter<Point>
+{
+	public override Point Read(BinaryReader reader)
+	{
+		return new(reader.ReadInt32(), reader.ReadInt32());
+	}
+
+	public override void Write(BinaryWriter writer, Point ins)
+	{
+		writer.Write(ins.X);
+		writer.Write(ins.Y);
+	}
+}
